1173674b2a3f57b92ea7d9a50235624607f70d73
linnaijun <g111234004@grad.ntue.edu.tw>
2024-07-20 23:18:12 +0800

    addtop

--- src/Album.js
+++ src/Album.js
@@ -2,13 +2,17 @@ import React from 'react';
 import AlbumBground from './AlbumBground';
 import './Album.css';
 
+
 function Album() {
   return (
     <div id="album" className="album-container">
+      
       <AlbumBground />
+      
       <div className="album-content">
         {/* Album content here */}
       </div>
+      
     </div>
   );
 }
