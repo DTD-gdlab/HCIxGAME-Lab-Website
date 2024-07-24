717e2578ba5b26d2b439d3a78c848b240e935d15
linnaijun <g111234004@grad.ntue.edu.tw>
2024-07-24 11:22:45 +0800

    autoloadimg

--- src/Album.js
+++ src/Album.js
@@ -8,30 +8,8 @@ import BottomWhiteWave from './img/BottomWhiteWave.png'; // Import the SVG for b
 import BottomGreyWave from './img/BottomGreyWave.png'; // Import the SVG for bottom
 import WelcomeImage from './img/Welcome.png'; // Import Welcome.png
 
-// Import Album Photos
-import AlbumPhoto01 from './img/Album/AlbumPhoto01.png';
-import AlbumPhoto02 from './img/Album/AlbumPhoto02.png';
-import AlbumPhoto03 from './img/Album/AlbumPhoto03.png';
-import AlbumPhoto04 from './img/Album/AlbumPhoto04.png';
-import AlbumPhoto05 from './img/Album/AlbumPhoto05.png';
-import AlbumPhoto06 from './img/Album/AlbumPhoto06.png';
-import AlbumPhoto07 from './img/Album/AlbumPhoto07.png';
-import AlbumPhoto08 from './img/Album/AlbumPhoto08.png';
-import AlbumPhoto09 from './img/Album/AlbumPhoto09.png';
-import AlbumPhoto10 from './img/Album/AlbumPhoto10.png';
-
-const albumPhotos = [
-  AlbumPhoto01,
-  AlbumPhoto02,
-  AlbumPhoto03,
-  AlbumPhoto04,
-  AlbumPhoto05,
-  AlbumPhoto06,
-  AlbumPhoto07,
-  AlbumPhoto08,
-  AlbumPhoto09,
-  AlbumPhoto10,
-];
+const importAll = (r) => r.keys().map(r);
+const albumPhotos = importAll(require.context('./img/Album', false, /\.png$/));
 
 function Album() {
   const [offset, setOffset] = useState(0);
